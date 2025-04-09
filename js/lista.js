0a7fd291cbca718716775eb8214bcb33e3dc522e
DaveDevop-Col <dsjs2307@gmail.com>
2025-04-09 16:47:56 +0000

    Detalle

--- js/lista.js
+++ js/lista.js
@@ -1,17 +1,18 @@
-function mostrarLista(listapokes){
+function mostrarLista(listapokes) {
     const app = document.getElementById("app");
     app.innerHTML = "";
     let pokes = "";
 
-    for (let i = 0; i < listapokes.length; i++){
+    for (let i = 0; i < listapokes.length; i++) {
         let id = listapokes[i].url.split("/")[6];
-        pokes = +=
-        <div class="c-lista-pokemon poke-${id}" onclick="mostrarDetalle('${pokemones[i].name}')">
-                <p>#${id}</p>
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png" width="auto" height="60" loading="lazy" alt="${pokemones[i].name}">
-                <p>${pokemones[i].name}</p>
+        pokes += `
+        <div class="c-lista-pokemon poke-${id}" onclick="mostrarDetalle('${listapokes[i].name}')">
+            <p>#${id}</p>
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png" width="auto" height="60" loading="lazy" alt="${listapokes[i].name}">
+            <p>${listapokes[i].name}</p>
         </div>`;
-        }
+    }
 
-    app.innerHTML = pokes
+    app.innerHTML = pokes;
+    app.appendChild(seccion);
 }
